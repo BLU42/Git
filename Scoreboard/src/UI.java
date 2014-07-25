0425213279230ca83016751a9e954457bfb848e1
BLU42 <DrBLU42@Gmail.com>
2014-07-25 00:12:21 -0400

    Scoreboard v1.2
    Added Capability to do team names

--- Scoreboard/src/UI.java
+++ Scoreboard/src/UI.java
@@ -12,6 +12,17 @@ import javax.swing.*;
  * UI.java
  * 
  * contains UI and main components for simple scoreboard program
+ * 
+ * File:
+ *  $Id: UI.java,v 1.2 2014/07/25 04:07:05 jem9375 Exp $
+ *  
+ * Revisions:
+ *  $Log: UI.java,v $
+ *  Revision 1.2  2014/07/25 04:07:05  jem9375
+ *  V 1.2
+ *
+ *  Added Team name output
+ *
  */
 
 /**
@@ -24,8 +35,10 @@ public class UI {
 	
 	private JPanel red; //left panel (red scoring)
 	private JPanel blue; // right panel (blue scoring)
-	private JPanel redButtons;
-	private JPanel blueButtons;
+	private JPanel redButtons; // Jpanel for + and - buttons
+	private JPanel redUpper; // Jpanel for upper text
+	private JPanel blueButtons; // Jpanel for + and - buttons
+	private JPanel blueUpper; // Jpanel for upper text
 	
 	private JLabel rScoreText;
 	private JLabel bScoreText;
@@ -36,8 +49,13 @@ public class UI {
 	private JButton bPlus; // blue score increase
 	private JButton bMinus; // blue score decrease
 	
+	private JTextField redTeam;
+	private JTextField blueTeam;
+	
 	private int rScore = 0;
 	private int bScore = 0;
+	private String redT = "";
+	private String blueT = "";
 	
 	public UI(){
 		start();
@@ -47,13 +65,43 @@ public class UI {
 	// SETUP FUNCTION
 	private void start(){
 		
-		//WINDOW SETUP
-		window = new JFrame("Simple Scoreboard v 1.1");
-		rScoreText = new JLabel("Red Score", SwingConstants.CENTER);
-		bScoreText = new JLabel("Blue Score", SwingConstants.CENTER);
+		//GENERAL SETUP
+		window = new JFrame("Simple Scoreboard v 1.2");
+		rScoreText = new JLabel("Red", SwingConstants.CENTER);
+		bScoreText = new JLabel("Blue", SwingConstants.CENTER);
 		rScoreRead = new JLabel(Integer.toString(rScore), SwingConstants.CENTER);
 		bScoreRead = new JLabel(Integer.toString(bScore), SwingConstants.CENTER);
 		
+		//TEAM TEXT BOX
+		redTeam = new JTextField(15);
+		ActionListener redReader = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				redT = redTeam.getText();
+				try {
+					write(3);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		};
+		redTeam.addActionListener(redReader);
+		
+		blueTeam = new JTextField(15);
+		ActionListener blueReader = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				blueT = blueTeam.getText();
+				try {
+					write(4);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		};
+		blueTeam.addActionListener(blueReader);
+		
+		//SCORE FONT SETUP
 		Font numberFont = new Font(rScoreRead.getFont().getName(), rScoreRead.getFont().getStyle(), 22);
 		rScoreRead.setFont(numberFont);
 		bScoreRead.setFont(numberFont);
@@ -128,7 +176,11 @@ public class UI {
 		
 		red = new JPanel();
 		red.setLayout(new BorderLayout());
-		red.add(rScoreText, BorderLayout.NORTH);
+		redUpper = new JPanel();
+		redUpper.setLayout(new BorderLayout());
+		redUpper.add(rScoreText, BorderLayout.NORTH);
+		redUpper.add(redTeam, BorderLayout.SOUTH);
+		red.add(redUpper, BorderLayout.NORTH);
 		red.add(rScoreRead, BorderLayout.CENTER);
 		redButtons = new JPanel();
 		redButtons.setLayout(new FlowLayout());
@@ -139,7 +191,11 @@ public class UI {
 		
 		blue = new JPanel();
 		blue.setLayout(new BorderLayout());
-		blue.add(bScoreText, BorderLayout.NORTH);
+		blueUpper = new JPanel();
+		blueUpper.setLayout(new BorderLayout());
+		blueUpper.add(bScoreText, BorderLayout.NORTH);
+		blueUpper.add(blueTeam, BorderLayout.SOUTH);
+		blue.add(blueUpper, BorderLayout.NORTH);
 		blue.add(bScoreRead, BorderLayout.CENTER);
 		blueButtons = new JPanel();
 		blueButtons.setLayout(new FlowLayout());
@@ -163,6 +219,16 @@ public class UI {
 		else if(side == 1){
 			PrintWriter outBlue = new PrintWriter("BlueScore.txt");
 			outBlue.printf("%d", bScore);
+			outBlue.close();
+		}
+		else if(side == 3){
+			PrintWriter outRed = new PrintWriter("RedTeam.txt");
+			outRed.printf("%s", redT);
+			outRed.close();
+		}
+		else if(side == 4){
+			PrintWriter outBlue = new PrintWriter("BlueTeam.txt");
+			outBlue.printf("%s", blueT);
 			outBlue.close();
 		}
 	}
